d178535dd13c2bf7067c2cf4bf7bab65605a3407
Diego.Tapia <95628579+Diego-Tapia@users.noreply.github.com>
2021-12-10 17:44:15 -0300

    fix joi

--- src/configs/environments/env.validations.ts
+++ src/configs/environments/env.validations.ts
@@ -3,10 +3,6 @@ import * as Joi from 'joi';
 export default Joi.object({
   PORT: Joi.number().required(),
   SQS_T_URL: Joi.string().required(),
-  SQS_USER: Joi.string().required(),
-  SQS_PASS: Joi.string().required(),
-  SQS_HOST: Joi.string().required(),
-  SQS_ENDPOINT_URL: Joi.string().required(),
   REGION: Joi.string().required(),
   COGNITO_USER_POOL_ID: Joi.string().required(),
   COGNITO_CLIENT_ID: Joi.string().required(),
