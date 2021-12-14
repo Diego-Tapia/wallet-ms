2f29d57aa5205608e545ef455842682c431801f5
Diego-Tapia <95628579+Diego-Tapia@users.noreply.github.com>
2021-12-14 13:11:27 -0300

    update method connection db (db mongo atlas remote)

--- src/configs/environments/env.validations.ts
+++ src/configs/environments/env.validations.ts
@@ -2,12 +2,7 @@ import * as Joi from 'joi';
 
 export default Joi.object({
   PORT: Joi.number().required(),
-  DB_ENGINE: Joi.string().required(),
-  DB_NAME: Joi.string().required(),
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().required(),
-  DB_USER: Joi.string().required(),
-  DB_PASS: Joi.string().required(),
+  DB_URI: Joi.string().required(),
   SQS_T_URL: Joi.string().required(),
   SQS_USER: Joi.string().required(),
   SQS_PASS: Joi.string().required(),
