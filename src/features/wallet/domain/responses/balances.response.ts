9e36dec16e97a8d19258809c06200a0974707554
martin.perez <martin.perez@ayi.group>
2021-12-15 16:01:03 -0300

    swagger wallet response description

--- src/features/wallet/domain/responses/balances.response.ts
+++ src/features/wallet/domain/responses/balances.response.ts
@@ -1,17 +1,20 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class GetBalancesResponse {
-    @ApiProperty()
-    total: number;
-
-    @ApiProperty()
-    balances: BalancesResponse[];
-}
-
 export class BalancesResponse {
     @ApiProperty()
     tokenId: string
 
     @ApiProperty()
     amount: number
+}
+
+export class GetBalancesResponse {
+    @ApiProperty()
+    total: number;
+
+    @ApiProperty({
+        description: 'Array of balances',
+        type: [BalancesResponse]
+    })
+    balances: BalancesResponse[];
 }
